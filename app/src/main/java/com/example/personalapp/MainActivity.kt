07552da615a74b4ba0ca8920a8db4bf38dd64dce
Antsa <antsarakotomanga12@gmail.com>
2025-10-29 23:37:23 +0100

    feat: Ajouter du splash screen à la page d'accueil et ajout de la page login

--- app/src/main/java/com/example/personalapp/MainActivity.kt
+++ app/src/main/java/com/example/personalapp/MainActivity.kt
@@ -6,11 +6,24 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.animation.AnimationUtils
+import kotlinx.coroutines.delay
+import android.os.Handler
+import android.os.Looper
+import com.example.personalapp.ui.login.LoginActivity
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Animation : Splash Screen
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000L)
     }
 }
 
