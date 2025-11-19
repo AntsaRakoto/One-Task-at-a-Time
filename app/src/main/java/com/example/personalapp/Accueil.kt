0b0ccfb6a794d1678c8f8c1c65641bd869e5f630
Antsa <antsarakotomanga12@gmail.com>
2025-11-19 20:11:31 +0100

    feat: conception du design de la page login

---
+++ app/src/main/java/com/example/personalapp/Accueil.kt
@@ -0,0 +1,13 @@
+package com.example.personalapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Accueil : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
