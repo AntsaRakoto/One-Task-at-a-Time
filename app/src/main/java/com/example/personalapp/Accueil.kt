162986fc74d0f3ff566cb80876adfba06e32bb30
Antsa <antsarakotomanga12@gmail.com>
2025-12-08 23:06:18 +0100

    feat: - Création de la table des taches - Implémentation des fonctionnalités pour avoir le to-do list de chaque projet

--- app/src/main/java/com/example/personalapp/Accueil.kt
+++ app/src/main/java/com/example/personalapp/Accueil.kt
@@ -1,12 +1,15 @@
 package com.example.personalapp
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.personalapp.data.AppDatabase
+import com.example.personalapp.utils.setupProfileLogout
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
@@ -24,6 +27,28 @@ class Accueil : AppCompatActivity() {
         btnActivityProjet = findViewById<Button>(R.id.btn_organisation)
         btnActivityTemps = findViewById<Button>(R.id.btn_temps_conseil)
         btnActivityProgres = findViewById<Button>(R.id.btn_avancement)
+
+        val profileBtn: ImageView = findViewById(R.id.profileBtn)
+        setupProfileLogout(this, profileBtn)
+
+
+        val prefs = getSharedPreferences("app_prefs", Context.MODE_PRIVATE)
+        val currentUserId = prefs.getLong("current_user_id", -1L) // -1 = pas connecté
+
+        if (currentUserId == -1L) {
+            // Pas d'utilisateur connecté → retourner au LoginActivity
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        } else {
+            // L'utilisateur est connecté → tu peux récupérer ses infos
+            lifecycleScope.launch {
+                val user = AppDatabase.getDatabase(this@Accueil).userDao().getUserById(currentUserId)
+                if (user != null) {
+                    userNameTextView.text = user.userName
+                }
+            }
+        }
 
         btnActivityTemps.setOnClickListener {
             val intent = Intent(this@Accueil, WeatherActivity::class.java)
